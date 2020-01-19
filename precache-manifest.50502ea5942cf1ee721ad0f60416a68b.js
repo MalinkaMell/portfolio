a107428b59d5b58618e96190e85c25c695ba9277
Maimell <malinka77@gmail.com>
2020-01-19 14:49:51 -0700

    Updates

---
+++ precache-manifest.50502ea5942cf1ee721ad0f60416a68b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7fa44d22fc3b3532ff482f355a53e9a",
+    "url": "/MalinkaMell/portfolio/index.html"
+  },
+  {
+    "revision": "e059a83c563dff2a2035",
+    "url": "/MalinkaMell/portfolio/static/css/main.d9f82c9f.chunk.css"
+  },
+  {
+    "revision": "466205392a29624db6ae",
+    "url": "/MalinkaMell/portfolio/static/js/2.91060deb.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/MalinkaMell/portfolio/static/js/2.91060deb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e059a83c563dff2a2035",
+    "url": "/MalinkaMell/portfolio/static/js/main.00131391.chunk.js"
+  },
+  {
+    "revision": "473698d3e17ff6165d75",
+    "url": "/MalinkaMell/portfolio/static/js/runtime-main.9f4160f3.js"
+  },
+  {
+    "revision": "ea3e17623ad2e48de000bb132ee37a48",
+    "url": "/MalinkaMell/portfolio/static/media/bg.ea3e1762.png"
+  }
+]);
